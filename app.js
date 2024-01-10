44f92aeee832887e3d301d476253900066acbb69
brunaverrone <bruna.verrone@sumup.com>
2024-01-10 11:40:57 -0300

    farmacia online

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port,() => {
-    console.log('API server listening on port ${port}')
+    console.log(`API server listening on port ${port}`)
 });
